f9c846958786c1adebb5a1b329e34dec25bdbac3
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-19 23:18:50 +0900

    no message

--- src/main/java/com/corinne/coin_data/service/QuestService.java
+++ src/main/java/com/corinne/coin_data/service/QuestService.java
@@ -4,12 +4,14 @@ import com.corinne.coin_data.websocket.model.Quest;
 import com.corinne.coin_data.websocket.repository.QuestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class QuestService {
     private final QuestRepository questRepository;
 
+    @Transactional
     public void checkQuest(Long userId){
         Quest quest = questRepository.findByUser_UserIdAndQuestNo(userId, 6).orElse(null);
 
